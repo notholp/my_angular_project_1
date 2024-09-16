261066ff959bbca2ce7b135d91493dac85ccfcc1
notholp <cimanptiphan@gmail.com>
2024-09-16 11:33:30 +0700

    first commit

--- my_angular_project_Ep8/src/app/app.component.ts
+++ my_angular_project_Ep8/src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
+import { Observable } from 'rxjs';
+
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -10,17 +12,13 @@ import { CommonModule } from '@angular/common';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'my_angular_project_1';
-  responseData: any;
+  title = 'my_angular_project_Ep8';
+  responseData$: Observable<any> | undefined;
   loadData : Boolean = false;
   constructor(private http: HttpClient){}
-
-    getData(){
-      this.http.get('https://jsonplaceholder.typicode.com/users').subscribe
-      ((data)=>{
-        this.responseData = data;
-        console.log(data);
+    
+  getData(){
+      this.responseData$ = this.http.get('https://jsonplaceholder.typicode.com/users');
         this.loadData = true;
-      })
     }
   }
