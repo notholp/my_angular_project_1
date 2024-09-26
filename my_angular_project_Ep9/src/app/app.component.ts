d7ab4de34d980ba3accaafcca5464dedba305300
notholp <cimanptiphan@gmail.com>
2024-09-26 14:20:12 +0700

    คอมมิทครั้งแรก

---
+++ my_angular_project_Ep9/src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, CommonModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'my_angular_project_Ep10';
+  responseData$: Observable<any> | undefined;
+  loadData : Boolean = false;
+  constructor(private http: HttpClient){}
+    
+  getData(){
+      this.responseData$ = this.http.get('https://jsonplaceholder.typicode.com/users');
+        this.loadData = true;
+    }
+  }
